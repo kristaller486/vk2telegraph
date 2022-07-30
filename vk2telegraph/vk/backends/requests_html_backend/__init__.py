6b60343f2aac091e00647300ea875bdeda6a855b
kristaller486 <kristalleroriginal@gamil.com>
2022-07-30 22:22:57 +0300

    add TODO

--- vk2telegraph/vk/backends/requests_html_backend/__init__.py
+++ vk2telegraph/vk/backends/requests_html_backend/__init__.py
@@ -9,7 +9,7 @@ def _is_valid_url(url: str) -> bool:
 
 def get(url: str) -> str:
     if not _is_valid_url(url):
-        raise ValueError('URL "{url}" not contain VK article')
+        raise ValueError('URL "{url}" not contain VK article') # TODO: add --force flag for ignore this exception
     with _rq.Session() as session:
         post = session.get(url)
     return post.text
